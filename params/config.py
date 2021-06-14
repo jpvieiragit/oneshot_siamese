68609a700dfd2a56eca53fbf1b865656bd94f77f
ispt-joel <joel@isportistics.com>
2021-06-14 16:44:26 -0300

    hotfix train param

--- params/config.py
+++ params/config.py
@@ -23,8 +23,8 @@ class Config:
     distanceLayer = True  # defines if the last layer uses a distance metric or a neuron output
     bceLoss = True  # If true uses Binary cross entropy. Else: contrastive loss
 
-    train_batch_size = 64
-    train_number_epochs = 400
+    train_batch_size = 128
+    train_number_epochs = 200
     lrate = 0.005
 
     ## Model save/load paths
