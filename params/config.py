c6dff6dce2de8e27e35d72460828a301e71a1afa
ispt-joel <joel@isportistics.com>
2021-06-15 13:19:43 -0300

    hotfix train param

--- params/config.py
+++ params/config.py
@@ -6,22 +6,22 @@ class Config:
     testing_dir = "../datasets/openlogo/test_split/test/"
 
     # Alexnet 224,224 following pytorch doc
-    im_w = 299
-    im_h = 299
+    im_w = 224
+    im_h = 224
 
     ## Model params
     # model = "alexnet"
     #model = "resnet"
     #model = "dense"
-    model = "inception"
-    #model = "vgg"
+    # model = "inception"
+    model = "vgg"
     #model = "squeeze"
     #model = "koch"
     #model = "tests"
 
     pretrained = False
     distanceLayer = True  # defines if the last layer uses a distance metric or a neuron output
-    bceLoss = False  # If true uses Binary cross entropy. Else: contrastive loss
+    bceLoss = True  # If true uses Binary cross entropy. Else: contrastive loss
 
     train_batch_size = 32
     train_number_epochs = 200
